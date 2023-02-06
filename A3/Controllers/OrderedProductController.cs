a8559a0e15c5364ac19ebe42a2afec474d4b6f13
Sahibjeet Singh (s3842316) <s3842316@student.rmit.edu.au>
2023-02-06 20:16:47 +1100

    TaskD Completed - View to display ordered products added

---
+++ A3/Controllers/OrderedProductController.cs
@@ -0,0 +1,41 @@
+﻿using A3.Data;
+using X.PagedList;
+using A3.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace A3.Controllers;
+
+public class OrderedProductController : Controller
+{
+    private readonly A3Context _context;
+
+    // Constructor
+    public OrderedProductController(A3Context context) => _context = context;
+
+    /**
+     * Code Adapted From - RMIT/WDT/Week5-Lectorial/MvcMovie
+     * and modified to include Paged List
+     */
+    [HttpGet]
+    public async Task<IActionResult> OrderedProduct(string? name, int? page = 1)
+    {
+        var productNames = _context.OrderedProducts.Include(x => x.Product).
+            Select(x => x.Product.Name).Distinct().OrderBy(x => x);
+
+        var orderedProducts = _context.OrderedProducts.Include(x => x.Product).Select(x => x);
+        if (!string.IsNullOrEmpty(name))
+            orderedProducts = orderedProducts.Where(x => x.Product.Name == name);
+
+        const int pageSize = 5;
+        return View(new OrderedProductsViewModel
+        {
+            Name = new SelectList(await productNames.ToListAsync()),
+            // paging
+            OrderedProduct = await orderedProducts.OrderBy(x => x.Product.Name).ToPagedListAsync(page, pageSize)
+        });
+        
+    }
+}
+
---
+++ A3/Models/ViewModels/OrderedProductsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using X.PagedList;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace A3.Models.ViewModels;
+
+/**
+ * Code Adapted From - RMIT/WDT/Week5-Lectorial/MvcMovie
+ */
+public class OrderedProductsViewModel
+{
+    public IPagedList<OrderedProduct> OrderedProduct { get; set; }
+    public SelectList Name { get; set; }
+    public string ProductName { get; set; }
+}
+
