45d8f40973b3ae4313c703e86bd7ec0bb99b1988
Sahibjeet Singh (s3842316) <s3842316@student.rmit.edu.au>
2023-02-06 17:24:05 +1100

    Display all products completed

---
+++ A3/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using A3.Data;
+using X.PagedList;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace A3.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly A3Context _context;
+
+    // Constructor
+    public ProductController(A3Context context) => _context = context;
+
+    [HttpGet]
+    public async Task<IActionResult> Product(int? page = 1)
+    {
+        // paging
+        const int pageSize = 5;
+        var pagedList = await _context.Products.ToPagedListAsync(page, pageSize);
+
+        return View(pagedList);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> AddProduct() => View();
+
+}
+
